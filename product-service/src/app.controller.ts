76d4749e80c1494d81cac42dd5bcc0a5e43c5109
Mohammed Aashik <aashikdevelop@gmail.com>
2026-07-23 16:29:59 +0400

    Update dependencies in api-gateway and product-service to include gRPC support and add microservices functionality. Implement gRPC method for fetching products in product-service and configure microservice options in the main bootstrap function.

--- product-service/src/app.controller.ts
+++ product-service/src/app.controller.ts
@@ -1,9 +1,38 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { GrpcMethod } from '@nestjs/microservices';
+
+interface Product {
+  id: number;
+  name: string;
+  price: number;
+}
+
+interface GetProductsResponse {
+  products: Product[];
+}
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
+
+  @GrpcMethod('ProductService', 'GetProducts')
+  getProducts(): GetProductsResponse {
+    return {
+      products: [
+        {
+          id: 1,
+          name: 'MacBook Pro',
+          price: 1999,
+        },
+        {
+          id: 2,
+          name: 'Mechanical Keyboard',
+          price: 120,
+        },
+      ],
+    };
+  }
 
   @Get()
   getHello(): string {
--- product-service/src/main.ts
+++ product-service/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3001);
+async function bootstrap(): Promise<void> {
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        package: 'product',
+        protoPath: join(__dirname, 'proto/product.proto'),
+        url: 'localhost:50051',
+      },
+    },
+  );
+  await app.listen();
+  console.log('Product Service gRPC server running on localhost:50051');
 }
 void bootstrap();
